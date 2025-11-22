ec99c2ec41ce6a63726744e921330a2b2b73388d
Jorge09829985 <jdv.escobar@yavirac.edu.ec>
2025-11-21 19:39:47 -0500

    rutas configuradas

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,23 @@
+import 'package:go_router/go_router.dart';
+import 'package:widgets_app/presentation/screens/screens.dart';
+
+// GoRouter configuration
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(),
+    ),
+
+    GoRoute(
+      path: '/buttons',
+      builder: (context, state) => const CardsScreens(),
+    ),
+
+    GoRoute(
+      path: '/cards',
+      builder: (context, state) => const ButtonsScreens(),
+    ),
+  ],
+);
--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
-import 'package:widgets_app/presentation/screens/buttons/buttons_screens.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -53,11 +53,13 @@ class _CustomListTile extends StatelessWidget {
       subtitle: Text(menuItem.subTitle),
       onTap: (){
 
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => const ButtonsScreens(), 
-          ),
-        );
+       // Navigator.of(context).push(
+        // MaterialPageRoute(
+         //   builder: (context) => const ButtonsScreens(), 
+          //),
+       // );
+
+       context.push(menuItem.link);
 
       },
     );
