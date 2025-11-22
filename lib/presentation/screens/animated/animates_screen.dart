fc8c45cce9cad71885f0c2e01e695251d2460f53
Jorge09829985 <jdv.escobar@yavirac.edu.ec>
2025-11-21 21:43:47 -0500

    screns creadas y barrill creadas

---
+++ lib/presentation/screens/animated/animates_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AnimatesScreen extends StatelessWidget {
+
+  static const name = 'animated_screen';
+
+  const AnimatesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,10 @@
+export 'package:widgets_app/presentation/screens/animated/animates_screen.dart';
+export 'package:widgets_app/presentation/screens/app_tutorial/app_tutorial.dart';
+export 'package:widgets_app/presentation/screens/infinite_scroll/infinite_scroll.dart';
+export 'package:widgets_app/presentation/screens/progres/progress_screen.dart';
+export 'package:widgets_app/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgets_app/presentation/screens/ui_controls/ui_controls_screens.dart';
+
 export 'package:widgets_app/presentation/screens/buttons/buttons_screens.dart';
 export 'package:widgets_app/presentation/screens/cards/cards_screens.dart';
 export 'package:widgets_app/presentation/screens/home/home_screen.dart';
